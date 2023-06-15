0fb0b5a4726b242eacfde213b215bcf993fedca6
MBaggett <baggett.michael@gmail.com>
2023-06-15 15:15:38 -0500

    Added js variable for website name and URL

---
+++ assets/scripts/var_url.js
@@ -0,0 +1,3 @@
+document.getElementById("web_url").href = "https://imageless-thought.github.io/";
+document.getElementById("web_url").alt = "Back to Honmepage";
+document.getElementById("web_url").innerHTML = "imageless-thought.github.io";
