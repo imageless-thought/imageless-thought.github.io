b266fa7cdd5574ac9e7b2b092d52a18d768e6295
MBaggett <baggett.michael@gmail.com>
2023-06-28 11:55:26 -0500

    Added itservices version

---
+++ assets/scripts/var_url_itservices.js
@@ -0,0 +1,5 @@
+
+const links = document.querySelectorAll("a.web_url");
+links.forEach(link => link.href = "https://itservices.cvad.unt.edu");
+links.forEach(link => link.alt = "Back to Homepage");
+links.forEach(link => link.innerHTML = "itservices.cvad.unt.edu");
