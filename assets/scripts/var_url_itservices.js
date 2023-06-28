92fb3af7c4676cbf90a5562d93365f74c6f55f74
MBaggett <baggett.michael@gmail.com>
2023-06-28 13:22:58 -0500

    Multi-site functionality added

--- assets/scripts/var_url_itservices.js
+++
@@ -1,5 +0,0 @@
-
-const links = document.querySelectorAll("a.web_url");
-links.forEach(link => link.href = "https://itservices.cvad.unt.edu");
-links.forEach(link => link.alt = "Back to Homepage");
-links.forEach(link => link.innerHTML = "itservices.cvad.unt.edu");
