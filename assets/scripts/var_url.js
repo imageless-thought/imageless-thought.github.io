1c5abf59710d40f2b13c8ec8f1ebe2eca7f3d9ba
MBaggett <baggett.michael@gmail.com>
2023-06-29 17:08:53 -0500

    Added sticky footer on all pages, added webname update for about and staff ms

--- assets/scripts/var_url.js
+++ assets/scripts/var_url.js
@@ -1,4 +1,3 @@
-
 const links = document.querySelectorAll("a.web_url");
 links.forEach(link => link.href = "https://imageless-thought.github.io/");
 links.forEach(link => link.alt = "Back to Homepage");
